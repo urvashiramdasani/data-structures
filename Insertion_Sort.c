df88e69a71bafd543d03d1f04d6b6c6b1f9c3e9c
Urvashi Ramdasani <54858768+urvashiramdasani@users.noreply.github.com>
2020-07-20 18:07:05 +0530

    Create Insertion_Sort.c

---
+++ Insertion_Sort.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+// function to swap two numbers
+void swap(int *num1, int *num2) {
+  int tmp = *num1;
+  *num1 = *num2;
+  *num2 = tmp;
+}
+
+// function to implement insertion sort recursively
+void insertionsort(int arr[], int n) {
+  if(n == 1) return;
+
+  // sort the last n-1 elements
+  insertionsort(arr, n-1);
+  int d = n-1;
+  while(d>0 && arr[d-1]>arr[d]) {
+    swap(&arr[d-1], &arr[d]);
+    d--;
+  }
+  arr[d+1] = 
+}
+
+int main(void) {
+
+  // inputting the size of array
+  printf("Enter the number of elements in array : ");
+  int n; scanf("%d", &n);
+  int arr[n];
+
+  // inputting the array
+  printf("\nEnter the elements in array: ");
+  for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+  
+  // Iterative Approach
+  for(int i=1; i<n; i++) {
+    int d = i;
+    while(d>0 && arr[d-1]>arr[d]) {
+      swap(&arr[d-1], &arr[d]);
+      d--;
+    }
+  }
+
+  printf("Array after Iterative Sort : ");
+  for(int i=0; i<n; i++) printf("%d ", arr[i]);
+
+  // Recursive Approach
+
+
+  return 0;
+}
