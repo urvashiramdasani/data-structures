0b184529a82f3a11a6541dd218d6eaac324753e0
Urvashi Ramdasani <54858768+urvashiramdasani@users.noreply.github.com>
2020-07-27 13:37:43 +0530

    Update Selection_Sort.c

--- Selection_Sort.c
+++ Selection_Sort.c
@@ -7,6 +7,11 @@ void swap(int *num1, int *num2) {
   *num2 = tmp;
 }
 
+// function to implement selection sort recursively
+void selectionsort(int *arr, int n) {
+  
+}
+
 int main(void) {
   
   // inputting the size of array
@@ -18,6 +23,20 @@ int main(void) {
   printf("\nEnter the elements in array: ");
   for(int i=0; i<n; i++) scanf("%d", &arr[i]);
 
+  // Iterative Approach
+  for(int i=0; i<n-1; i++) {
+    int min = i;
+    for(int j=i+1; j<n; j++) {
+      if(arr[j] < arr[min]) min = j;
+    }
+    swap(&arr[i], &arr[min]);
+  }
+
+  printf("\nArray after Iterative Sort : ");
+  for(int i=0; i<n; i++) printf("%d ", arr[i]);
+
+  // Recursive Approach
+  selectionsort(arr, n);
   
   return 0;
 }
